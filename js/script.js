95f0d60807d8b6b2c6374488bf9487a6064ba698
KUNALKAUSHIKBORA <85491418+KUNALKAUSHIKBORA@users.noreply.github.com>
2021-07-21 21:36:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
-var options = {
-    "key": "rzp_test_9WPw6kfBxKrq1t", // Enter the Key ID generated from the Dashboard
-    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-    "currency": "INR",
-    "name": "Acme Corp",
-    "description": "Test Transaction",
-    "image": "https://example.com/your_logo",
-    "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-    "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
-    "prefill": {
-        "name": "Gaurav Kumar",
-        "email": "gaurav.kumar@example.com",
-        "contact": "9999999999"
-    },
-    "notes": {
-        "address": "Razorpay Corporate Office"
-    },
-    "theme": {
-        "color": "#3399cc"
-    }
-};
-var rzp1 = new Razorpay(options);
-document.getElementById('rzp-button1').onclick = function(e){
-    rzp1.open();
-    e.preventDefault();
+const rzp = new Razorpay({
+ key_id: "rzp_test_9WPw6kfBxKrq1t",
+ key_secret: "bVvUDMkk0cnWcVRu4KuZlIhR",
+})
+
+const rzpOrder = await rzp.orders.create({
+ amount: amount * 100, // rzp format with paise
+ currency: 'INR',
+ receipt: "receipt#1" //Receipt no that corresponds to this Order,
+ payment_capture: true,
+ notes: {
+  orderType: "Pre"
+ } //Key-value pair used to store additional information
+})
+// To create recurring subscription
+const subscriptionObject = {
+ plan_id: PLAN_ID,
+ total_count: 60,
+ quantity: 1,
+ customer_notify: 1,
+ notes,
 }
+const subscription = await rzp.subscriptions.create(subscriptionObject)
